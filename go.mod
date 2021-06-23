79dae809014d38f8f94566741816ca5076fb4886
Arvin Lee <gnimoaix857@gmail.com>
2021-06-24 02:26:58 +0700

    Use gorilla mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/xiaoming857/chatroom
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	// TODO: Implement chatroom
-	print(`Hello Chatroom!`)
+	r := mux.NewRouter()
+	r.HandleFunc(`/`, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `Hello World!`)
+	})
+	log.Println(`Serve starting at localhost:4444`)
+	http.ListenAndServe(`:4444`, r)
 }
